fe32501f7765177def008181086c9e737c14154b
sokoli3 <sokoli3@illinois.edu>
2018-11-07 16:40:20 -0600

    bubblewrap

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -33,7 +33,16 @@ public class Sorting {
      */
     @SuppressWarnings("unused")
     private static int[] bubbleSort(final int[] array) {
-        return null;
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 1; j < array.length - i; j++) {
+                    if (array[j] < array[j - 1]) {
+                        int temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            }
+            return array;
     }
 
     /**
